653abc4fcf359c6f8c5789144d81d12eded0e313
Anoop Sarkar <anoop+github@cs.sfu.ca>
2016-12-02 11:01:42 -0800

    updated to new nltk api

--- drawtree.py
+++ drawtree.py
@@ -3,5 +3,6 @@ import sys
 if __name__ == '__main__':
     for line in sys.stdin:
         input = line.strip()
-        tree = nltk.Tree(input)
+        tree = nltk.Tree.fromstring(input)
+        print(tree)
         tree.draw()
